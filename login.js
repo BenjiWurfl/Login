75f3febe78988f327761856b93f32e08aec070f9
Benjamin Würflinger <benjamin.wuerflinger@htl-wels.at>
2023-11-13 10:24:17 +0100

    Button redirecting

--- login.js
+++ login.js
@@ -1,3 +1,22 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
 function redirect() {
     window.location.href = "https://thinkwisenotes.webflow.io/app";
+}
+
+function redirectSignUp() {
+    window.location.href = "https://benjiwurfl.github.io/Sign-Up/";
 }
