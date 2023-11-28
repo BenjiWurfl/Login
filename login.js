e9862dbceb39404b83041c7ac6758cd6e8564256
Noah-Pesendorfer <noah.pesendorfer@htl-wels.at>
2023-11-28 17:23:00 +0100

    Login Funktionen updated

--- login.js
+++ login.js
@@ -1,53 +1,53 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-        import { getDatabase, get, ref, child } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
-        import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
-        import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-analytics.js";
+import { getDatabase, get, ref, child } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-analytics.js";
 
-        const firebaseConfig = {
-          apiKey: "AIzaSyBe7d9bllq8RnmI6xxEBk3oub3qogPT2aM",
-          authDomain: "thinkwise-c7673.firebaseapp.com",
-          databaseURL: "https://thinkwise-c7673-default-rtdb.europe-west1.firebasedatabase.app",
-          projectId: "thinkwise-c7673",
-          storageBucket: "thinkwise-c7673.appspot.com",
-          messagingSenderId: "37732571551",
-          appId: "1:37732571551:web:9b90a849ac5454f33a85aa",
-          measurementId: "G-8957WM4SB7"
-        };
-      
-        const app = initializeApp(firebaseConfig);
-        const analytics = getAnalytics(app);
-        const db = getDatabase();
-        const auth = getAuth(app);
-        const dbref = ref(db);
+const firebaseConfig = {
+  apiKey: "AIzaSyBe7d9bllq8RnmI6xxEBk3oub3qogPT2aM",
+  authDomain: "thinkwise-c7673.firebaseapp.com",
+  databaseURL: "https://thinkwise-c7673-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "thinkwise-c7673",
+  storageBucket: "thinkwise-c7673.appspot.com",
+  messagingSenderId: "37732571551",
+  appId: "1:37732571551:web:9b90a849ac5454f33a85aa",
+  measurementId: "G-8957WM4SB7"
+};
 
-        let EmailInput = document.getElementById('emailInput');
-        let PasswordInput = document.getElementById('passwordInput');
-        
-        let LoginForm = document.getElementById('LoginForm');
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getDatabase();
+const auth = getAuth(app);
+const dbref = ref(db);
 
-        let LoginUser = evt => {
-            evt.preventDefault();
+let EmailInput = document.getElementById('emailInput');
+let PasswordInput = document.getElementById('passwordInput');
 
-            signInWithEmailAndPassword(auth, EmailInput.value, PasswordInput.value)
-            .then((credentials)=>{
-                get(child(dbref, 'UsersAuthList/' + credentials.user.uid))
-                .then((snapshot) => {
-                  if(snapshot.exists){
-                    sessionStorage.setItem("user-info", JSON.stringify({
-                      firstname: snapshot.val().firstname,
-                      lastname: snapshot.val().lastname
-                    }))
-                    sessionStorage.setItem('user-creds', JSOn.stringify(credentials.user));
-                    window.location.href = 'www.thinkwisenotes.webflow.io/app'
-                  }
-                })
+let LoginForm = document.getElementById('LoginForm');
 
-            })
-            .catch((error) =>{
-                alert(error.message)
-                console.log(error.code);
-                console.log(error.message);
-            })
-        }
+let LoginUser = evt => {
+    evt.preventDefault();
 
-        LoginForm.addEventListener('submit', LoginUser);
+    signInWithEmailAndPassword(auth, EmailInput.value, PasswordInput.value)
+    .then((credentials)=>{
+        get(child(dbref, 'UsersAuthList/' + credentials.user.uid))
+        .then((snapshot) => {
+          if(snapshot.exists){
+            sessionStorage.setItem("user-info", JSON.stringify({
+              firstname: snapshot.val().firstname,
+              lastname: snapshot.val().lastname
+            }))
+            sessionStorage.setItem('user-creds', JSOn.stringify(credentials.user));
+            window.location.href = 'http://thinkwisenotes.webflow.io/app'
+          }
+        })
+
+    })
+    .catch((error) =>{
+        alert(error.message)
+        console.log(error.code);
+        console.log(error.message);
+    })
+}
+
+LoginForm.addEventListener('submit', LoginUser);
