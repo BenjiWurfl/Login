c4f42a3c99309b0980697b93acad7033036b1603
Noah-Pesendorfer <noah.pesendorfer@htl-wels.at>
2023-11-28 16:42:52 +0100

    Funktionen Login Test

--- login.js
+++ login.js
@@ -1,23 +1,53 @@
-/*import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+        import { getDatabase, get, ref, child } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
+        import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+        import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-analytics.js";
 
-const auth = getAuth();
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed up 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
-  */
+        const firebaseConfig = {
+          apiKey: "AIzaSyBe7d9bllq8RnmI6xxEBk3oub3qogPT2aM",
+          authDomain: "thinkwise-c7673.firebaseapp.com",
+          databaseURL: "https://thinkwise-c7673-default-rtdb.europe-west1.firebasedatabase.app",
+          projectId: "thinkwise-c7673",
+          storageBucket: "thinkwise-c7673.appspot.com",
+          messagingSenderId: "37732571551",
+          appId: "1:37732571551:web:9b90a849ac5454f33a85aa",
+          measurementId: "G-8957WM4SB7"
+        };
+      
+        const app = initializeApp(firebaseConfig);
+        const analytics = getAnalytics(app);
+        const db = getDatabase();
+        const auth = getAuth(app);
+        const dbref = ref(db);
 
-//function redirect() {
-//    window.location.href = "https://thinkwisenotes.webflow.io/app";
-//}
+        let EmailInput = document.getElementById('emailInput');
+        let PasswordInput = document.getElementById('passwordInput');
+        
+        let LoginForm = document.getElementById('LoginForm');
 
-function redirectSignUp() {
-    window.location.href = "https://benjiwurfl.github.io/Sign-Up/";
-}
+        let LoginUser = evt => {
+            evt.preventDefault();
+
+            signInWithEmailAndPassword(auth, EmailInput.value, PasswordInput.value)
+            .then((credentials)=>{
+                get(child(dbref, 'UsersAuthList/' + credentials.user.uid))
+                .then((snapshot) => {
+                  if(snapshot.exists){
+                    sessionStorage.setItem("user-info", JSON.stringify({
+                      firstname: snapshot.val().firstname,
+                      lastname: snapshot.val().lastname
+                    }))
+                    sessionStorage.setItem('user-creds', JSOn.stringify(credentials.user));
+                    window.location.href = 'www.thinkwisenotes.webflow.io/app'
+                  }
+                })
+
+            })
+            .catch((error) =>{
+                alert(error.message)
+                console.log(error.code);
+                console.log(error.message);
+            })
+        }
+
+        LoginForm.addEventListener('submit', LoginUser);
